a28b50ea2ea3300d8afbd571a3ed4a9a3002f701
legends-killer <958775729ljz@gmail.com>
2021-04-06 12:56:39 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch, Link } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div>
+        <Link to="/">Home </Link>
+        <Link to="/about">About Us </Link>
+      </div>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route component={Home} />
+      </Switch>
+    </main>
+  )
 }
 
-export default App;
+export default App
